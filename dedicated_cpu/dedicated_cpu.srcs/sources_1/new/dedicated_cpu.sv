3c3578c3d66f8c10a0ebb16725093c924e1bf2a0
Aspera <sciabole@icloud.com>
2026-03-06 22:36:20 +0900

    rebuild

--- dedicated_cpu/dedicated_cpu.srcs/sources_1/new/dedicated_cpu.sv
+++ dedicated_cpu/dedicated_cpu.srcs/sources_1/new/dedicated_cpu.sv
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
 
-module dedicated_cpu (
+module sum_accumulator (
     input        clk,
     input        rst,
     output [7:0] out
 );
 
-    logic asrcsel, aload, outsel, alt10;
+    logic asrcsel, aload, outsel, alt10, sumsrcsel, sumload, alusel;
 
     control_unit U_CONTROL_UNIT (.*);
 
@@ -22,7 +22,10 @@ module control_unit (
     input        alt10,
     output logic asrcsel,
     output logic aload,
-    output logic outsel
+    output logic outsel,
+    output logic sumload,
+    output logic sumsrcsel,
+    output logic alusel
 );
 
     typedef enum logic [2:0] {
@@ -30,7 +33,8 @@ module control_unit (
         S1 = 1,
         S2 = 2,
         S3 = 3,
-        S4 = 4
+        S4 = 4,
+        S5 = 5
     } state_t;  // typedef 재정의, enum 문자를 숫자화
     state_t c_state, n_state;
 
@@ -45,22 +49,31 @@ module control_unit (
     always_comb begin
         n_state = c_state;
         asrcsel = 0;
-        aload   = 0;
-        outsel  = 1;
+        aload = 0;
+        outsel = 0;
+        sumload = 0;
+        sumsrcsel = 0;
+        alusel = 0;
         case (c_state)
             S0: begin
                 asrcsel = 0;
-                aload   = 1;
-                outsel  = 0;
+                aload = 1;
+                outsel = 0;
+                sumload = 1;
+                sumsrcsel = 0;
+                alusel = 0;
                 n_state = S1;
             end
 
             S1: begin
-                
-                    // asrcsel = ; dont care
-                    aload   = 0;
-                    outsel  = 0;
-                    if(alt10) begin
+
+                asrcsel = 0;  //dont care
+                aload = 0;
+                outsel = 0;
+                sumload = 0;
+                sumsrcsel = 0;
+                alusel = 0;
+                if (alt10) begin
                     n_state = S2;
                 end else begin
                     n_state = S4;
@@ -68,24 +81,42 @@ module control_unit (
             end
 
             S2: begin
-                // asrcsel = ; dont care
-                aload   = 0;
-                outsel  = 1;
+                asrcsel = 0;  //dont care
+                aload = 0;
+                outsel = 0;
+                sumload = 1;
+                sumsrcsel = 1;
+                alusel = 0;
                 n_state = S3;
             end
 
             S3: begin
                 asrcsel = 1;
-                aload   = 1;
-                outsel  = 0;
+                aload = 1;
+                outsel = 0;
+                sumload = 0;
+                sumsrcsel = 0;
+                alusel = 1;
                 n_state = S1;
             end
 
             S4: begin
-                // asrcsel = ; dont care
-                aload   = 0;
-                outsel  = 1;
-                n_state = S4;
+                asrcsel = 0;  //dont care
+                aload = 0;
+                outsel = 1;
+                sumload = 0;
+                sumsrcsel = 0;
+                alusel = 0;
+                n_state = S5;
+            end
+
+            S5: begin
+                asrcsel = 0;
+                aload = 0;
+                outsel = 1;
+                sumload = 0;
+                sumsrcsel = 0;
+                alusel = 0;
             end
         endcase
     end
@@ -97,19 +128,35 @@ module datapath (
     input              rst,
     input              asrcsel,
     input              aload,
+    input              sumload,
     input              outsel,
+    input              sumsrcsel,
+    input              alusel,
     output             alt10,
     output logic [7:0] out
 );
 
-    logic [7:0] w_aluout, w_muxout, w_regout;
-    assign out = (outsel) ? w_regout : 8'hz;
+    logic [7:0]
+        w_aluout, w_muxout, w_regout, w_sumreg_out, w_sum_muxout, w_sum_mux2alu;
+    assign out = (outsel) ? w_sumreg_out : 8'h0;
 
-    mux_2x1 U_ASRCMUX (
+    mux_2x1 U_ASRCMUX_A (
         .a(0),
         .b(w_aluout),
         .asrcsel(asrcsel),
         .mux_out(w_muxout)
+    );
+    mux_2x1 U_ASRCMUX_B (
+        .a(0),
+        .b(w_aluout),
+        .asrcsel(sumsrcsel),
+        .mux_out(w_sum_muxout)
+    );
+    mux_2x1 U_ASRCMUX_SUM (
+        .a(w_sumreg_out),
+        .b(8'd1),
+        .asrcsel(alusel),
+        .mux_out(w_sum_mux2alu)
     );
     areg U_AREG (
         .clk(clk),
@@ -118,15 +165,21 @@ module datapath (
         .aload(aload),
         .reg_out(w_regout)
     );
+    areg U_BREG (
+        .clk(clk),
+        .rst(rst),
+        .reg_in(w_sum_muxout),
+        .aload(sumload),
+        .reg_out(w_sumreg_out)
+    );
     alu U_ALU (
         .a(w_regout),
-        .b(8'h1),
+        .b(w_sum_mux2alu),
         .alu_out(w_aluout)
     );
-
-    alt10_comp U_ALT10(
-        .in_data(w_regout), 
-        .alt10(alt10)
+    alt10_comp U_ALT10 (
+        .in_data(w_regout),
+        .alt10  (alt10)
     );
 
 endmodule
@@ -185,6 +238,6 @@ module alt10_comp (
     output alt10
 );
 
-    assign alt10 = (in_data < 10);
+    assign alt10 = (in_data <= 10);
 
 endmodule
