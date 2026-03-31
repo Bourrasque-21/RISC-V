76698844b0a86cd08b4b30dc46276b9920cf6986
Aspera <sciabole@icloud.com>
2026-03-31 22:33:42 +0900

    gpi/gpo

--- RV32I_mcu/RV32I_mcu.srcs/sources_1/new/gpo_01.sv
+++ RV32I_mcu/RV32I_mcu.srcs/sources_1/new/gpo_01.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module apb_gpo01 (
+module APB_GPO (
     input               PCLK,
     input               PRESET,
     input        [31:0] PADDR,
@@ -8,28 +8,31 @@ module apb_gpo01 (
     input               PENABLE,
     input               PWRITE,
     input               PSEL,
-    output logic        PREADY,
     output logic [31:0] PRDATA,
-    output logic [15:0] GPO_OUT
+    output logic        PREADY,
+    output logic [ 7:0] GPO_OUT
 );
 
-    localparam [11:0] GPO_CTL_ADDR = 12'h0000;
-    localparam [11:0] GPO_ODATA_ADDR = 12'h0004;
-    logic [15:0] GPO_ODATA_REG, GPO_CTL_REG;
+    localparam [11:0] GPO_CTL_ADDR   = 12'h000;
+    localparam [11:0] GPO_ODATA_ADDR = 12'h004;
 
-    assign PREADY = (PENABLE & PSEL) ? 1'b1 : 1'b0;
+    logic [7:0] GPO_ODATA_REG, GPO_CTL_REG;
 
-    assign PRDATA = (PADDR[11:0]==GPO_CTL_ADDR) ? {16'h0000, GPO_CTL_REG} :
-                    (PADDR[11:0]==GPO_ODATA_ADDR) ? {16'h0000, GPO_ODATA_REG}:32'hxxxx_xxxx;
+    assign PREADY = PENABLE & PSEL;
+
+    assign PRDATA = (PADDR[11:0] == GPO_CTL_ADDR)   ? {24'h000000, GPO_CTL_REG} :
+                    (PADDR[11:0] == GPO_ODATA_ADDR) ? {24'h000000, GPO_ODATA_REG} :
+                                                      32'h0000_0000;
+
     always_ff @(posedge PCLK, posedge PRESET) begin
         if (PRESET) begin
-            GPO_ODATA_REG <= 16'h0;
-            GPO_CTL_REG   <= 16'h0;
+            GPO_ODATA_REG <= 8'h00;
+            GPO_CTL_REG   <= 8'h00;
         end else begin
             if (PREADY & PWRITE) begin
                 case (PADDR[11:0])
-                    GPO_CTL_ADDR:   GPO_CTL_REG <= PWDATA[15:0];
-                    GPO_ODATA_ADDR: GPO_ODATA_REG <= PWDATA[15:0];
+                    GPO_CTL_ADDR:   GPO_CTL_REG <= PWDATA[7:0];
+                    GPO_ODATA_ADDR: GPO_ODATA_REG <= PWDATA[7:0];
                 endcase
             end
         end
@@ -37,8 +40,8 @@ module apb_gpo01 (
 
     genvar i;
     generate
-        for (i = 0; i < 16; i++) begin
-            assign GPO_OUT[i] = (GPO_CTL_REG[i]) ? GPO_ODATA_REG[i] : 1'b0;
+        for (i = 0; i < 8; i++) begin
+            assign GPO_OUT[i] = GPO_CTL_REG[i] ? GPO_ODATA_REG[i] : 1'b0;
         end
     endgenerate
 
